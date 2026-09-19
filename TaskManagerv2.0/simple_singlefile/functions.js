8dab429fdb52933bed48b26b597285d6b17cd271
Gashomon <142756955+Gashomon@users.noreply.github.com>
2026-09-19 13:52:35 +0800

    updated edit function

--- TaskManagerv2.0/simple_singlefile/functions.js
+++ TaskManagerv2.0/simple_singlefile/functions.js
@@ -4,7 +4,11 @@ let openTasks = [];
 let closeTasks = [];
 let toTask = "";
 let historyIndex = 0; 
-let index = historyIndex
+let currentTaskID = historyIndex;
+
+const limit = 300;
+const editEvent = new Event('editTask');
+const deleteEvent = new Event('deleteTask');
 
 class Task{
   constructor(taskName){
@@ -18,6 +22,7 @@ class Task{
     this.state = "open";
     
     this.createTask(taskName);
+    this.task.id = this.id;
     this.doneButton.textContent = "Close";
     this.editButton.textContent = "Edit";
     this.deleteButton.textContent = "Delete";
@@ -33,7 +38,6 @@ class Task{
     this.task.appendChild(this.editButton);
     this.task.appendChild(this.deleteButton);
 
-
     this.doneButton.onclick = () => {this.toggleDone()};
     this.editButton.onclick = () => {this.editTask()};
     this.deleteButton.onclick =() => {this.deleteTask()};
@@ -41,51 +45,73 @@ class Task{
 
   createTask(name){
     this.name.textContent = name;
-    loadHistoryIndex();
     this.id = historyIndex++;
     Tasks[this.id] = this;
   }
 
   toggleDone(){
-    if (this.doneButton.textContent == 'Open'){
-      this.doneButton.textContent = "Close";
+    if (this.state == 'open'){
+      this.state = 'close';
+      this.doneButton.textContent = "Open";
+      this.editButton.disabled = true;
+      this.task.className = "homeMain taskMain darken";
+      document.getElementById("doneTasks").appendChild(this.task);
     }
     else{
-      this.doneButton.textContent = "Open";
+      this.state = 'open';
+      this.doneButton.textContent = "Close";
+      this.editButton.disabled = false;
+      this.task.className = "homeMain taskMain";
+      document.getElementById("openTasks").appendChild(this.task);
     }
+  }
+
+  editTask(){
+    if (toTask == "add") return; 
+    currentTaskID = this.id;
+    toTask = "edit";
+    document.getElementById("actionBox").style.display = "";
+    document.getElementById("actionButton").textContent = "Edit Task";
+    this.task.className = "homeMain taskMain selected";
   }
 
   deleteTask(){
     this.task.remove();
+    historyIndex--;
+    delete Tasks[this.id];
   }
 }
 
 function addTask(){
+  if(toTask == "edit") return;
   toTask = "add";
   document.getElementById("actionBox").style.display = "";
-}
-
-function deleteTask(){
-  toTask = "delete";
-  document.getElementById("actionBox").style.display = "";
+  document.getElementById("actionButton").textContent = "Add Task";
 }
 
 function editTask(){
-  toTask = "edit";
-  document.getElementById("actionBox").style.display = "";
+  
 }
 
-function toggleFinished(){
-   
+function delteTask(){
 }
+
 
 function acceptAction(){
   switch (toTask) {
     case "add":
-      console.log(document.getElementById("openTasks"));
-      document.getElementById("openTasks").appendChild(new Task(taskName=document.getElementById("inputText").value).task);
+      if (historyIndex >= limit) {
+        window.alert("Reached limit. delete somme tasks first");
+        return;
+      }
+        document.getElementById("openTasks").appendChild(new Task(taskName=document.getElementById("inputText").value).task);
       break;
-  
+    
+    case "edit":
+      document.getElementById(currentTaskID).firstChild.textContent = document.getElementById("inputText").value;
+      document.getElementById(currentTaskID).className = "homeMain taskMain";
+      break;
+
     default:
       window.alert("Error: no set Action");
       break;
@@ -97,15 +123,18 @@ function cancelAction(){
   toTask = "";
   document.getElementById("inputText").value = "";
   document.getElementById("actionBox").style.display = "none";
+  document.getElementById(currentTaskID).className = "homeMain taskMain";
 }
 
 function deleteAllTask() {
   document.getElementById("openTasks").textContent="";
+  document.getElementById("doneTasks").textContent="";
   Tasks = {};
   localStorage.removeItem(historyIndex);
 }
 
 function saveTasks() {
+
 }
 
 function loadTasks() {
